8f7b3a722d2d75faa3b5bea5d2a218753f708735
iasmimaraujol <iasmim.engenharia@gmail.com>
2022-06-09 14:22:22 -0300

    classe Pessoa pronta, talvez coloque eu novas funcionalidades no futuro

--- Pessoa.java
+++ Pessoa.java
@@ -3,5 +3,28 @@ class Pessoa{
    private String nome;
    private int idade;
 
-   
+   public Pessoa(String sexo, String nome, int idade){
+       setSexo(sexo);
+       setNome(nome);
+       setIdade(idade);
+   }   
+
+   public void setSexo(String sexo){
+       this.sexo = sexo;
+   }
+   public String getSexo(){
+       return this.sexo;
+   }
+   public void setNome(String nome){
+       this.nome = nome;
+   }
+   public String getNome(){
+       return this.nome;
+   }
+   public void setIdade(int idade){
+       this.idade = idade;
+   }
+   public int getIdade(){
+       return this.idade;
+   }
 }
